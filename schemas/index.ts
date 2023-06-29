d0eb6b08f821fd3a4a3b104a6c447d545d96abf3
Shreyas <telishreyas10@gmail.com>
2023-06-28 19:26:21 -0700

    initial commit

---
+++ schemas/index.ts
@@ -0,0 +1,10 @@
+import about from './about';
+import brands from './brands';
+import contact from './contact';
+import experience from './experience';
+import projects from './projects';
+import skills from './skills';
+import testimonials from './testimonials';
+import workExperience from './workExperience';
+
+export const schemaTypes = [about,brands,contact,experience,projects,skills,testimonials,workExperience]
